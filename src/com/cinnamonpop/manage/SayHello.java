8dbb6cc5f46786092a2f0092546c33c8ee2567e2
Ed Chen <ed.chen@ensighten.com>
2012-11-24 21:18:50 -0800

    Say good nite

--- src/com/cinnamonpop/manage/SayHello.java
+++ src/com/cinnamonpop/manage/SayHello.java
@@ -9,6 +9,6 @@ package com.cinnamonpop.manage;
 public class SayHello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git!");
+		System.out.println("Hello Git, Here's good nite!");
 	}
 }
